e416239aef18a7c21565b8f31909f0ff8798c8c3
Zarf-42 <smulch@hotmail.com>
2025-02-10 15:50:54 -0800

    Finalizing 1.0 version of app

--- dice_roller/lib/dice_roller.dart
+++ dice_roller/lib/dice_roller.dart
@@ -24,24 +24,25 @@ class _DiceRollerState extends State<DiceRoller> {
 
   @override
   Widget build(context) {
-    return Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-          Image.asset(
-            'assets/images/dice-$currentDiceRoll.png',
-            width: 200),
+    return GestureDetector(
+      onTap: rollDice,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset('assets/images/dice-$currentDiceRoll.png', width: 200),
           const SizedBox(height: 20),
           TextButton(
-            onPressed: rollDice, 
+            onPressed: rollDice,
             style: TextButton.styleFrom(
               foregroundColor: Colors.white,
               textStyle: const TextStyle(
                 fontSize: 28,
               ),
             ),
-            child: const Text('Roll Dice'),
-            )
+            child: const Text('Roll it!'),
+          )
         ],
-        );
+      ),
+    );
   }
 }
